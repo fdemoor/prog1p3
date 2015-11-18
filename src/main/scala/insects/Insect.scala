3d3de1ee81cc97fc328316e45cae960db2252fe2
Simon Bihel <simon.bihel@outlook.com>
2015-11-18 22:23:57 +0100

    Work on ants

--- src/main/scala/insects/Insect.scala
+++ src/main/scala/insects/Insect.scala
@@ -1,40 +1,72 @@
 package insects
 
 import javax.swing.ImageIcon
+import colony._
 
-class Insect(posx: Int, posy: Int, img: String) {
+abstract class Insect(posX: Int, posY: Int, img: String, _armor: Int = 1) {
   val icon: ImageIcon = new ImageIcon("src/main/resources/img/" + img + ".png")
-  val im = icon.getImage()
+  val im = icon.getImage
 
-  private var varx: Int = posx
-  private var vary: Int = posy
-  private var vardx: Int = 1
-  private var vardy: Int = 1
+  private var x: Int = posX
+  private var y: Int = posY
+  private var dx: Int = 1
+  private var dy: Int = 1
+  private var armor = _armor
 
-  def x(): Int = { varx }
-  def y(): Int = { vary }
-  def dx(): Int = { vardx }
-  def dy(): Int = { vardy }
+  def getX: Int = x
+  def getY: Int = y
+  def getDX: Int = dx
+  def getDY: Int = dy
+  def getArmor: Int = armor
 
-    // Update the position considering speed
-  def move (): Unit = {
-        varx += this.dx
-        vary += this.dy
-    }
-
-    // Increase speed
-  def accelerate (ax:Int, ay:Int): Unit = {
-    vardx += ax
-    vardy += ay
+  def setX(newX: Int) { x = newX }
+  def setY(newY: Int) { y = newY }
+  def setArmor(newArmor: Int) {
+    armor = newArmor
+    if (armor >= 0) this = null
   }
 
-    // Decrease speed
-  def decelerate (ax:Int, ay:Int): Unit = {
-    vardx -= ax
-    vardy -= ay
+  /** Update the position considering speed */
+  def move() {
+    x += dx
+    y += dy
+  }
+
+  /** Increase speed */
+  def accelerate(ax:Int, ay:Int) {
+    dx += ax
+    dy += ay
+  }
+
+  /** Decrease speed */
+  def decelerate(ax:Int, ay:Int) {
+    dx -= ax
+    dy -= ay
+  }
+
+  def moveActions()
+}
+
+abstract class Ant(posX: Int, posY: Int, img: String, colony: Colony, cost: Int, _armor: Int = 1)
+  extends Insect(posX, posY, "ant_" + img, _armor) {
+
+  private val Cost = cost
+  private val Colony = colony
+
+  def getCost: Int = Cost
+  def getColonny: Colony = Colony
+
+  def addFood(amount: Int = 1) { Colony.setFoodAmount(Colony.getFoodAmount + amount) }
+}
+
+class HarvesterAnt(posX: Int, posY: Int, colony: Colony) extends Ant(posX, posY, "harvester", colony, 2) {
+  override def moveActions() {
+   addFood()
   }
 }
 
-class Ant() extends Insect(1, 1, "fourmi") {
-  val hello = "hello"
+class ThrowerAnt(posX: Int, posY: Int, colony: Colony) extends Ant(posX, posY, "harvester", colony, 2) {
+  override def moveActions() {
+   addFood()
+  }
 }
