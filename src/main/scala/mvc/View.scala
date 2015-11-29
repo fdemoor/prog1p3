2ad1dc5ee429c3d2e6de41233300960e61ba38b6
florestan.demoor <florestan.demoor@gmail.com>
2015-11-29 21:54:32 +0100

    Improved drawing projectiles, removed commented code, View is sooo nice now !

--- src/main/scala/mvc/View.scala
+++ src/main/scala/mvc/View.scala
@@ -9,17 +9,13 @@ import scala.swing.event._
 import scala.swing.Panel
 
 
-/** Paint the place and all that is inside */
-
-
 class View(_controller: Controller, grid: Grid, _Colony: Colony) {
 
   private val controller: Controller = _controller
-  //private val places: List[Place] = placesList
   private val Colony: Colony = _Colony
   
   
-  
+  /** Paint the places and all that is inside */
   def paintGrid(gridGame: Grid, g: Graphics2D, peer:java.awt.Component): Unit = {
     
     for (p <- gridGame.places) {
@@ -54,7 +50,8 @@ class View(_controller: Controller, grid: Grid, _Colony: Colony) {
     /* Returns the current position of the mouse (or null if it's not over the panel */
     def getPos = peer.getMousePosition()
 
-    // MENU
+
+    /* CREATING MENU */
     val menu = new UIButtonMenu(Nil: List[UIButton])
 
     val harvesterIcon: ImageIcon = new ImageIcon(getClass.getResource("/img/ant_harvester.png"))
@@ -100,9 +97,6 @@ class View(_controller: Controller, grid: Grid, _Colony: Colony) {
     val byeIcon: ImageIcon = new ImageIcon(getClass.getResource("/img/remover.png"))
     val byeButton = new UIButton(byeIcon, 10 + menu.buttons.head.width*10, 10, 4 ,2, "bye")
     menu.add(byeButton)
-    
-    val selectBoxWidth: Int = 66
-    val selectBoxHeight: Int = 66 + 40
 
 
     override def paintComponent(g: Graphics2D) = {
@@ -112,82 +106,31 @@ class View(_controller: Controller, grid: Grid, _Colony: Colony) {
       if (pos != null) g.drawString("x: "+pos.x+" y: "+pos.y, size.width-85, 15)
 
 
+      /* BUTTON MENU */
       menu.paint(g, peer)
 
-      /*// DRAW PLACES AND INSECTS //
-      g.setColor(Color.black)
-      for (p <- placesList) {
-        val box = new geom.GeneralPath
-        box.moveTo(p.x, p.y)
-        box.lineTo(p.x + p.width, p.y)
-        box.lineTo(p.x + p.width, p.y + p.height)
-        box.lineTo(p.x, p.y + p.height)
-        box.lineTo(p.x, p.y)
-        g.draw(box)
-        g.drawImage(p.im, p.x, p.y, peer)
-        val xtoCenter: Int = (p.width - selectBoxWidth) / 2
-        val ytoCenter: Int = (p.height - selectBoxWidth) / 2
-        for (bee <- p.bees) {
-          g.drawImage(bee.im, bee.x.toInt + xtoCenter, bee.y + ytoCenter, peer)
-        }
-        if (p.isAntIn) {
-          if (p.ant.isContainer && p.ant.asInstanceOf[BodyguardAnt].ant.isDefined) {
-            val underAnt = p.ant.asInstanceOf[BodyguardAnt].ant.get
-            g.drawImage(underAnt.im, underAnt.x.toInt + xtoCenter, underAnt.y + ytoCenter, peer)
-          }
-          g.drawImage(p.ant.im, p.ant.x.toInt + xtoCenter, p.ant.y + ytoCenter, peer)
-        }
-      }*/
-      
+
+      /* PLACES */
       paintGrid(grid, g, peer)
 
+
       /* BOTTOM INFO BAR */
+      
+      // FOOD
       val foodIcon: ImageIcon = new ImageIcon(getClass.getResource("/img/food.png"))
       g.drawImage(foodIcon.getImage, 10, size.height-26, peer)
       g.drawString(" " + Colony.foodAmount.toString, 10+16, size.height-10)
       
+      // SCORE
       val scoreIcon: ImageIcon = new ImageIcon(getClass.getResource("/img/score.png"))
       g.drawImage(scoreIcon.getImage, 10+32+40, size.height-26, peer)
       g.drawString(" " + Colony.scoreAmount.toString, 10+48+40, size.height-10)
 
-      /** Draw a Selecter Box and print cost and armor of the unit, red if selected, black otherwise */
-      /*def drawSelectedBox(x: Int, y: Int, bool: Boolean, cost: Int, armor: Int, icon: ImageIcon) = {
-        val currentSelectBox = new geom.GeneralPath
-        currentSelectBox.moveTo(x, y)
-        currentSelectBox.lineTo(x + selectBoxWidth, y)
-        currentSelectBox.lineTo(x + selectBoxWidth,  y + selectBoxHeight)
-        currentSelectBox.lineTo(x, y + selectBoxHeight)
-        currentSelectBox.lineTo(x, y)
-        if (bool) {
-          g.setColor(Color.red)
-        } else {
-          g.setColor(Color.black)
-        }
-        g.drawString(" Cost: " + cost.toString, x, y + 20)
-        g.drawString(" Armor: " + armor.toString, x, y + 40)
-        g.draw(currentSelectBox)
-        val currentIm = icon.getImage
-        g.drawImage(currentIm, x, y + 40, peer)
-      }*/
 
-      /*drawSelectedBox(10 + selectBoxWidth * 0, 10, controller.harvesterSelected, 2, 1, harvesterIcon)
-      drawSelectedBox(10 + selectBoxWidth * 1, 10, controller.shortThrowerSelected, 3, 1, shortThrowerIcon)
-      drawSelectedBox(10 + selectBoxWidth * 2, 10, controller.longThrowerSelected, 3, 1, longThrowerIcon)
-      drawSelectedBox(10 + selectBoxWidth * 3, 10, controller.fireSelected, 5, 1, fireIcon)
-      drawSelectedBox(10 + selectBoxWidth * 4, 10, controller.scubaSelected, 5, 1, scubaIcon)
-      drawSelectedBox(10 + selectBoxWidth * 5, 10, controller.wallSelected, 4, 4, wallIcon)
-      drawSelectedBox(10 + selectBoxWidth * 6, 10, controller.ninjaSelected, 6, 1, ninjaIcon)
-      drawSelectedBox(10 + selectBoxWidth * 7, 10, controller.hungrySelected, 4, 1, hungryIcon)
-      drawSelectedBox(10 + selectBoxWidth * 8, 10, controller.queenSelected, 6, 2, queenIcon)
-      drawSelectedBox(10 + selectBoxWidth * 9, 10, controller.bodyGuardSelected, 6, 2, bodyGuardIcon)
-      drawSelectedBox(10 + selectBoxWidth * 10, 10, controller.byeSelected, 4, 2, byeIcon)*/
-
-
-       // DRAW PROJECTILES
+      /* PROJECTILES */
       g.setColor(Color.red)
       for (p <- Projectiles.projectiles) {
-        val circle = new geom.Ellipse2D.Double(p.x, p.y + 30, 8, 8)
-        g.draw(circle)
+        g.fillOval(p.x, p.y + 30, 8, 8)
       }
     }
 
@@ -197,44 +140,7 @@ class View(_controller: Controller, grid: Grid, _Colony: Colony) {
       case e: MouseReleased =>
         menu.mouseAction(getPos.x, getPos.y)
         controller.placeClicked((getPos.x, getPos.y), menu)
-
-       /*if (10 + selectBoxWidth * 0 <= getPos.x && getPos.x < 10 + selectBoxWidth * 1 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.harvesterClicked()
-        } else if (10 + selectBoxWidth * 1 <= getPos.x && getPos.x < 10 + selectBoxWidth * 2 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.shortThrowerClicked()
-        } else if (10 + selectBoxWidth * 2 <= getPos.x && getPos.x < 10 + selectBoxWidth * 3 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.longThrowerClicked()
-        } else if (10 + selectBoxWidth * 3 <= getPos.x && getPos.x < 10 + selectBoxWidth * 4 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.fireClicked()
-        } else if (10 + selectBoxWidth * 4 <= getPos.x && getPos.x < 10 + selectBoxWidth * 5 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.scubaClicked()
-        } else if (10 + selectBoxWidth * 5 <= getPos.x && getPos.x < 10 + selectBoxWidth * 6 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.wallClicked()
-        } else if (10 + selectBoxWidth * 6 <= getPos.x && getPos.x < 10 + selectBoxWidth * 7 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.ninjaClicked()
-        } else if (10 + selectBoxWidth * 7 <= getPos.x && getPos.x < 10 + selectBoxWidth * 8 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.hungryClicked()
-        } else if (10 + selectBoxWidth * 8 <= getPos.x && getPos.x < 10 + selectBoxWidth * 9 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.queenClicked()
-        } else if (10 + selectBoxWidth * 9 <= getPos.x && getPos.x < 10 + selectBoxWidth * 10 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.bodyGuardClicked()
-        } else if (10 + selectBoxWidth * 10 <= getPos.x && getPos.x < 10 + selectBoxWidth * 11 &&
-            10 <= getPos.y && getPos.y < 10 + selectBoxHeight) {
-          controller.byeClicked()
-        } else {
-          
-        }
-      case _: FocusLost => repaint()*/
+      case _: FocusLost => repaint()
     }
   }
 
